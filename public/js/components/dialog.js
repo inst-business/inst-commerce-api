58e4b1b76b9145676cc9ab171648b7f1740c9c43
Btoann <nguyenbaotoan2001@gmail.com>
2023-06-26 00:15:20 +0700

    enhance css

--- public/js/components/dialog.js
+++ public/js/components/dialog.js
@@ -1,11 +1,11 @@
 import qs from '../consts.js'
 
 export const __DialogBtns = Array.from(qs.$a('[data-toggle = dialog]'))
-export const __Dialogs = Array.from(qs.$a('dialog.appDialog'))
+export const __Dialogs = Array.from(qs.$a('dialog.Dialog'))
 
 __DialogBtns.map(btn => btn.addEventListener('click', function toggleDialog () {
   const target = btn.dataset.dialogTarget
-  const targetDialog = qs.$o(`dialog.appDialog${target}`)
+  const targetDialog = qs.$o(`dialog.Dialog${target}`)
   if (!targetDialog) {
     return
   }
--- public/js/pages/courses/index.js
+++ public/js/pages/courses/index.js
@@ -1,17 +1,17 @@
 
-const dialogBtns = document.querySelectorAll('button.appItem__actions-delete')
+const dialogBtns = document.querySelectorAll('button.Item__actions-delete')
 Array.from(dialogBtns).map(btn => btn.onclick = e => {
   const target = e.target.dataset.dialogTarget,
         dialog = document.querySelector(target)
   if (!dialog) return
-  const item = e.target.closest('.appItem'),
+  const item = e.target.closest('.Item'),
         _id = e.target.dataset.formId,
         input = dialog.querySelector('input[name="ids[]"]'),
-        title = item.querySelector('.appItem__name').innerText,
+        title = item.querySelector('.Item__name').innerText,
         quote = /*html*/`You really want to delete <strong>${title}</strong> course?`,
         url = `/courses/i?_method=DELETE`
   input.value = _id
-  dialog.querySelector('.appDialog__body').innerHTML = quote
-  const form = dialog.querySelector('.appDialog__form')
+  dialog.querySelector('.Dialog__body').innerHTML = quote
+  const form = dialog.querySelector('.Dialog__form')
   form.action = url
 })
