d06d0155e48e6df958a76c17788b909a1a7ca550
Btoann <nguyenbaotoan2001@gmail.com>
2023-08-15 22:58:50 +0700

    select all checkbox

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 import { __DialogBtns } from './components/dialog.js'
 import { __PopoverTriggers } from './components/popover.js'
 import { __ViewSwitcher } from './components/viewSwitcher.js'
+import { __SelectAlls } from './components/selectAll.js'
 
 import './config/onload.js'
